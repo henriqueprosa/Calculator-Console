00a3a05e2ad39c6e50e98b6101ea566a460ceaf8
henriqueprosa <henriqueprosa.dev@gmail.com>
2023-01-02 11:42:24 -0300

    Application menu creation and data entry

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,34 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What operation do you want to do?");
+            Console.WriteLine("1 - Addition");
+            Console.WriteLine("2 - Subtraction");
+            Console.WriteLine("3 - Multiplication");
+            Console.WriteLine("4 - Division \n");
+
+            int operation = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int result;
+                     
+
+        }
+              
+    }
+}
